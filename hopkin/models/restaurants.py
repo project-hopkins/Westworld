14bffb5be29d7a5539c63e5b5ac07c90030811ad
aramrattan <arlina.ramrattan@gmail.com>
2017-11-23 16:38:40 -0500

    added collection class and static methods

---
+++ hopkin/models/restaurants.py
@@ -0,0 +1,21 @@
+from hopkin.app import flask_db
+from bson.objectid import ObjectId
+
+class Restaurant:
+    collection_name = 'restaurants'
+
+    @staticmethod
+    def get_all():
+        return flask_db.db[Restaurant.collection_name]
+
+    @staticmethod
+    def get_by_id(restaurant_id):
+        return flask_db.db[Restaurant.collection_name].find_one({'_id': restaurant_id})
+
+    @staticmethod
+    def insert(new_restaurant):
+        return flask_db.db[Restaurant.collection_name].insert_one(new_restaurant).inserted_id
+
+    @staticmethod
+    def remove(restaurant_id):
+        flask_db.db[Restaurant.collection_name].delete_one({'_id': ObjectId(restaurant_id)})
